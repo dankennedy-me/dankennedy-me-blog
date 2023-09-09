fa9c707c5010375723dbbaf392a106ff73a1892e
Daniel Kennedy <hello@dankennedy.me>
2023-09-09 09:23:09 -0400

    added login redirect to cms

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/login',
+            destination: 'https://cms.dankennedy.me',
+            permanent: true,
+          },
+        ]
+      },
+}
 
 module.exports = nextConfig
